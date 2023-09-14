ddbc3da12df424861438da8aa5d971c62295cc77
sejoDjoseon <sergio.morales93@gmail.com>
2023-09-14 20:42:02 +0200

    install react-native-image-picker

--- src/screens/Main/CreatePost/TakePhoto/TakePhoto.tsx
+++ src/screens/Main/CreatePost/TakePhoto/TakePhoto.tsx
@@ -3,13 +3,48 @@ import * as React from 'react'
 import {StyleSheet, Text, View} from 'react-native'
 import {Button} from 'react-native-paper'
 import {WriteDescriptionProps} from '..'
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker'
 
 export default () => {
   const navigation = useNavigation<WriteDescriptionProps['navigation']>()
+  const openCamera = () => {
+    launchCamera(
+      {
+        mediaType: 'photo',
+      },
+      photo => {
+        console.log(photo)
+      },
+    )
+  }
+
+  const openLibrary = () => {
+    launchImageLibrary(
+      {
+        mediaType: 'photo',
+      },
+      image => {
+        console.log(image)
+      },
+    )
+  }
 
   return (
     <View>
       <Text style={styles.title}>TakePhoto</Text>
+      <Button
+        onPress={() => {
+          openCamera()
+        }}>
+        Open Camera
+      </Button>
+      <Button
+        onPress={() => {
+          openLibrary()
+        }}>
+        Open Library
+      </Button>
+
       <Button
         onPress={() => {
           navigation.navigate('WriteDescription')
